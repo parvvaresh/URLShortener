db532ff95949f997ea906c69aa2e5faa85ae1d42
alireza parvaresh <parvvaresh@gmail.com>
2025-09-18 14:12:15 +0330

    add service logic

--- internal/service/service.go
+++ internal/service/service.go
@@ -5,3 +5,29 @@ import (
 	"url-shortener/internal/repository"
 	"url-shortener/pkg/utils"
 )
+
+type URLService struct {
+	repo *repository.URLRepository
+}
+
+func NewURLService(repo *repository.URLRepository) *URLService {
+	return &URLService(repo : repo)
+}
+
+func (s *URLService) Shorten(original string) (string, string) {
+	shortCode := utils.GenerateCode(6)
+	err := s.repo.Save(shortCode, original)
+
+	if err != nil {
+		return "", err
+	}
+	return shortCode, nil
+}
+
+func (s *URLService) Resolve(code string) (string, error) {
+	url, err := s.repo.Get(code)
+	if err != nil {
+		return "", errors.New("Not Found")
+	}
+	return url, nil
+}
